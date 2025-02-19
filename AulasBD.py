5ae655540176bdbfbe316319358eac6be6da5c79
Aline <aline.gdm18@gmail.com>
2025-02-19 14:58:29 -0300

    Adicionando comando Insert

--- AulasBD.py
+++ AulasBD.py
@@ -16,8 +16,15 @@ cursor = conn.cursor()
 #cursor.execute('ALTER TABLE usuario ADD COLUMN telefoni INT;')
 #cursor.execute('ALTER TABLE usuario RENAME COLUMN telefoni TO telefone;')
 
+#Inserindo dados
+#Para adicionar mais de um dado, basta repetir o comando abaixo, comentando o anterior
+#cursor.execute('INSERT INTO usuario (id, nome, endereco, email, telefone) VALUES (1, "João", "Rua 1","joao@gmail.com",11123456789);')
+#cursor.execute('INSERT INTO usuario (id, nome, endereco, email, telefone) VALUES (2, "Maria", "Rua 2","maria@gmail.com",11123456789);')
+#cursor.execute('INSERT INTO usuario (id, nome, endereco, email, telefone) VALUES (3, "Jose", "Rua 3","jose@gmail.com",11123456789);')
+#cursor.execute('INSERT INTO usuario (id, nome, endereco, email, telefone) VALUES (4, "Marcia", "Rua 4","marcia@gmail.com",11123456789);')
+
 #Excluindo tabela
-cursor.execute('DROP TABLE produtos;')
+#cursor.execute('DROP TABLE produtos;')
 
 
 #envio de comandos SQL
