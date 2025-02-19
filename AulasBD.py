ff6d888ba0a2c1096f54a2da3013a33f369c12be
Aline <aline.gdm18@gmail.com>
2025-02-19 14:24:40 -0300

    Adicionando comandos Create, Alter e Drop table

--- AulasBD.py
+++ AulasBD.py
@@ -4,7 +4,21 @@ import sqlite3
 conn = sqlite3.connect('DB')
 cursor = conn.cursor()
 
-cursor.execute('')
+#Criando tabela
+#Para criar outra tabela, basta repetir o comando abaixo, comentando o anterior
+#cursor.execute('CREATE TABLE usuarios(id INT, nome VARCHAR(100),endereco VARCHAR(100), email VARCHAR(100));')
+#cursor.execute('CREATE TABLE produtos(id INT, nome VARCHAR(100),endereco VARCHAR(100), email VARCHAR(100));')
+
+
+#Alterando tabela
+#Para adicionar mais de uma coluna, basta repetir o comando abaixo, comentando o anterior
+#cursor.execute('ALTER TABLE usuarios RENAME TO usuario;')
+#cursor.execute('ALTER TABLE usuario ADD COLUMN telefoni INT;')
+#cursor.execute('ALTER TABLE usuario RENAME COLUMN telefoni TO telefone;')
+
+#Excluindo tabela
+cursor.execute('DROP TABLE produtos;')
+
 
 #envio de comandos SQL
 conn.commit()
